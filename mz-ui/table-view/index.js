452df5d229ca845f3bbe9aefdc93343cc8d098e4
Marco Tang <tzzwpqwe@vip.qq.com>
2025-11-07 11:06:30 +0800

    保留唯一MZUI

--- mz-ui/table-view/index.js
+++
@@ -1,6 +0,0 @@
-import { staticDictionary } from '../static-dictionary'
-export const setWidth = (rowBtns) => {
-  let strLen = rowBtns.map(item => staticDictionary[item]).join('').length
-  let num = rowBtns.length
-  return strLen * 14 + (num * 16) + 36
-}
